903f2b6f550a9a6d4c6d590f57587d07507dcc19
Tommy Mac <tommyaznmac2k@gmail.com>
2023-08-25 00:10:08 -0700

    updated profolio with some added links

--- src/App.js
+++ src/App.js
@@ -7,19 +7,26 @@ import typescript_logo from "./images/typescript_logo.png"
 import react_logo from "./images/react_logo.png"
 import uw_logo from './images/uw_logo.png'
 import coding_dojo_logo from './images/coding_dojo.png'
+import javascript_logo from './images/javascript_logo.png'
+import python_logo from './images/python_logo.png'
+import aws_logo from './images/aws_logo.png'
+import docker_logo from './images/docker_logo.png'
+import kubernetes_logo from './images/kubernetes_logo.png'
+import { useState } from 'react';
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false)
   return (
-    <div className="App">
+    <div className={darkMode ? "dark" : ""}>
       <header className="App-header">
         <title>Tommy Mac Portfolio</title>
       </header>
-      <main className='bg-white px-10'>
+      <main className='bg-white px-10 dark:bg-gray-900 dark:text-white'>
         <section className="min-h-screen">
           <nav className="py-10 mb-12 flex justify-between">
             <h1 className="text-xl">Developed By</h1>
             <ul className='flex items-center'>
-              <li><BsFillMoonStarsFill className='cursor-pointer text-xl'/></li>
+              <li><BsFillMoonStarsFill className='cursor-pointer text-xl' onClick={()=> setDarkMode(!darkMode)}/></li>
               <li className='bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8' href="#">Resume</li>
             </ul>
           </nav>
@@ -32,7 +39,7 @@ function App() {
               <div className='p-10 w-1/4'>
                 <h1 className='text-center text-3xl'>About Me:</h1>
                 <div>
-                  <p className='text-md py-5 leading-8 text-gray-800'>
+                  <p className='text-md py-5 leading-8'>
                     Graduated from University of Washington With a B.S in Biology. Started my career
                     working in wet-lab at LabCorp where I later transition to Invitae and stayed with Invitae
                     for ~ 3 years. I initially came to Invitae working in lab and was graciously offered an opportunity
@@ -43,14 +50,14 @@ function App() {
                     <img src={bitmoji_waving} layout="fill"/>
                 </div>
                 <div className='flex text-5xl gap-16 py-3 text-gray-600 ml-7 mt-7'>
-                  <AiFillLinkedin />
-                  <AiFillYoutube />
-                  <AiFillGithub />
+                  <a target="blank" href="https://www.linkedin.com/in/tommy-mac-4a5087122/"><AiFillLinkedin className='cursor-pointer'/></a>
+                  <AiFillYoutube className='cursor-pointer'/>
+                  <a target="blank" href="https://github.com/heybro200"><AiFillGithub className='cursor-pointer'/></a>
                 </div>
               </div>
               <div className='w-1/4 text-center'>
                 <h1 className='text-center text-3xl p-10'>Education:</h1>
-                <p className='text-md py-5 leading-8 text-gray-800'>
+                <p className='text-md py-5 leading-8'>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
                 sed do eiusmod tempor incididunt ut labore et dolore magna 
                 aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
@@ -61,19 +68,35 @@ function App() {
                 officia deserunt mollit anim id est laborum.
                 </p>
                 <div className="flex justify-center relative mt-20">
-                  <img src={uw_logo} className='w-30 h-20 justify-center' layout='fill'/>
-                  <img src={coding_dojo_logo} className='w-30 h-20 justify-center' layout='fill'/>
+                  <img src={uw_logo} className='w-30 h-20' layout='fill'/>
+                  <img src={coding_dojo_logo} className='w-30 h-20' layout='fill'/>
                 </div>
                 
               </div>
               <div className='w-1/4 ml-10'>
                 <h2 className='text-center text-3xl p-10'>Technologies</h2>
-                <div>
-                  <ul className='ml-10'>
-                    <img src={django_logo} className='w-40 h-40' />
-                    <img src={react_logo} className='w-40 h-40'/>
-                    <img src={typescript_logo} className='w-40 h-40'/>
-                  </ul>
+                <div className='flex relative justify-center'>
+                  <img src={javascript_logo} className='w-20 h-20'/>
+                  <img src={python_logo} className='w-30 h-20'/>
+                </div>
+                <div className='flex relative justify-center'>
+                  <img src={django_logo} className='w-20 h-20' />
+                  <img src={react_logo} className='w-20 h-20'/>
+                  <img src={typescript_logo} className='w-20 h-20'/>
+                </div>
+                <p className='text-md py-5 leading-8'>
+                Senectus et netus et malesuada fames ac. Consequat interdum 
+                varius sit amet mattis vulputate enim nulla aliquet. Proin 
+                gravida hendrerit lectus a. Ut faucibus pulvinar elementum 
+                integer. Duis at tellus at urna condimentum mattis pellentesque. 
+                In hendrerit gravida rutrum quisque non tellus. Donec massa sapien 
+                faucibus et molestie ac feugiat sed. Adipiscing elit ut aliquam 
+                purus sit amet. Feugiat in ante metus dictum.
+                </p>
+                <div className='flex relative justify-center py-6 m-4'>
+                  <img src={aws_logo} className='w-30 h-20' />
+                  <img src={kubernetes_logo} className='w-30 h-20'/>
+                  <img src={docker_logo} className='w-30 h-20'/>
                 </div>
                 
               
@@ -97,14 +120,6 @@ function App() {
               for ~ 3 years. I initially came to Invitae working in lab and was graciously offered an opportunity
               to take another role as Software Engineer where I participated in full development cycle (design/implementations) of 3 successful products
             </p>
-          </div>
-          <div>
-            <h2 className='text-center text-3xl p-10'>Technologies</h2>
-            <div className='flex py-3 gap-16 justify-center mt-20 relative mx-auto bg-gradient-to-b from-teal-500 rounded-full overflow-hidden'>
-                <img src={django_logo} className='w-40 h-40' />
-                <img src={react_logo} className='w-40 h-40'/>
-                <img src={typescript_logo} className='w-40 h-40'/>
-            </div>
           </div>
         </section>
       </main>
